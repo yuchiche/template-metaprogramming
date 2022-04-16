425af4b898e0adb6699572ce526f7acca9f655ca
yuchiche <yuchi.chen@intel.com>
2022-04-16 23:02:03 +0800

    fix "2. expression and different type", wrong for:
    1. C style string
    2. using as a whole expression

--- macro.c
+++ macro.c
@@ -5,7 +5,7 @@ int ia = 10086, ib = 10001;
 double da = 1.0086, db = 1.0001;
 char stra[] = "10086", strb[] = "10001";
 
-#define max(a, b) a < b ? b : a
+#define max(a, b) (a) < (b) ? (b) : (a)
 
 int main()
 {
@@ -13,6 +13,7 @@ int main()
     printf("max(da, db) = %.4lf\n", max(da, db));
     printf("max(stra, strb) = %s\n", max(stra, strb));
     printf("max(ia == ib, ia) = %d\n", max(ia == ib, ia));
+    printf("10010 + max(ia, ib) = %d\n", 10010 + max(ia, ib));
 
     return 0;
 }
