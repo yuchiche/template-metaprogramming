83fc663a74e237a8882a45e8b52ae2716d5e6114
yuchiche <yuchi.chen@intel.com>
2022-04-17 11:49:03 +0800

    fix "1. different types", compilation error for:
    1. not less than comparable types

--- template.cpp
+++ template.cpp
@@ -5,8 +5,61 @@ int ia = 10086, ib = 10001;
 double da = 1.0086, db = 1.0001;
 char stra[] = "10086", strb[] = "10001";
 
+class MyClass
+{
+public:
+    int n;
+    bool operator>(const MyClass &lhs) { return n < lhs.n; }
+    friend std::ostream& operator<<(std::ostream &os, const MyClass &lhs)
+    {
+        os << "MyClass::n = " << lhs.n;
+        return os;
+    }
+};
+MyClass mca{10086}, mcb{10001};
+
+template <typename From, typename To>
+struct Convertible
+{
+    static void auxiliary(To);
+    template <typename U, typename = decltype(auxiliary(std::declval<U>()))>
+    static std::true_type test(void*);
+    template <typename U>
+    static std::false_type test(...);
+
+    static constexpr bool value = decltype(test<From>(nullptr))::value;
+};
+
+template<bool condition, typename IfType, typename ElseType>
+struct IfElse
+{
+    using Type = IfType;
+};
+
+template<typename IfType, typename ElseType>
+struct IfElse<false, IfType, ElseType>
+{
+    using Type = ElseType;
+};
+
+template <typename T1, typename T2>
+struct CommonType
+{
+    using Convert2T1 = Convertible<T2, T1>;
+    using Convert2T2 = Convertible<T1, T2>;
+    static_assert(Convert2T1::value || Convert2T2::value, "No conversion between T1 and T2");
+    static_assert(!(Convert2T1::value && Convert2T2::value), "Bidirectional conversion between T1 and T2");
+    using Type = typename IfElse<Convert2T1::value, T1, T2>::Type;
+};
+
 template <typename T>
-T max(T a, T b)
+struct CommonType<T, T>
+{
+    using Type = T;
+};
+
+template <typename T1, typename T2, typename R = CommonType<T1, T2>>
+typename R::Type max(T1 a, T2 b)
 {
     return a < b ? b : a;
 }
@@ -22,10 +75,20 @@ int main()
     std::cout << "max(ia, ib) = " << max(ia, ib) << std::endl;
     std::cout << "max(da, db) = " << max(da, db) << std::endl;
     std::cout << "max(stra, strb) = " << max(stra, strb) << std::endl;
-    // std::cout << "max(ia == ib, ia) = " << max(ia == ib, ia) << std::endl;
     std::cout << "10010 + max(ia, ib) = " << 10010 + max(ia, ib) << std::endl;
     std::cout << "max(ia++, ib++) = " << max(ia++, ib++) << ", ia = " << ia << ", ib = " << ib << std::endl;
     std::cout << "ia = " << ia << ", ib = " << ib << std::endl;
+
+    // test for bidirectional convertible types
+    // std::cout << "max(ia == ib, ia) = " << max(ia == ib, ia) << std::endl;
+    // std::cout << "max(ia, ia == ib) = " << max(ia, ia == ib) << std::endl;
+
+    // test for not convertible types
+    // std::cout << "max(ia, mcb) = " << max(ia, mcb) << std::endl;
+    // std::cout << "max(mca, ib) = " << max(mca, ib) << std::endl;
+
+    // test for not less than comparable types
+    // std::cout << "max(mca, mcb) = " << max(mca, mcb) << std::endl;
 
     return 0;
 }
